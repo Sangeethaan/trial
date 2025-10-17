1a4d55d6256e2eda3e0e2f9bfe82f50b3f41d781
Sangeethaan <sangeethagowda944@gmail.com>
2025-10-17 10:45:08 +0530

    Update geminiai.js

--- Backend/utils/geminiai.js
+++ Backend/utils/geminiai.js
@@ -10,7 +10,7 @@ const getGeminiApiResponse = async (prompt) => {
   }
 
   try {
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
     const result = await model.generateContent(prompt);
     const response = await result.response;
     return response.text();
